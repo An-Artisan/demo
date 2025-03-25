0405c8b578df0e1e53530f441dfb9bc728ba122d
liuqiang <liuqiang@smzdm.com>
2025-03-25 16:18:38 +0800

    feature: 优化处理

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,16 @@ use lib\schedule\ScheduledTask;
 //crontab -e 配置  /usr/local/opt/php@7.1/bin/php /Users/artisan/PHP/demo/artisan schedule:run
 //  /usr/local/opt/php@7.1/bin/php /Users/artisan/PHP/demo/artisan 修改自己的php目录和项目目录
 
+/**
+ * Kernel.php
+ * Description: 控制台命令行管理
+ * Created by: artisan
+ * Class Kernel
+ * @package app\Console
+ * @copyright Copyright (c) 2018-2025 artisan
+ * @license g1090035743@gmail.com
+ * @version 1.0
+ */
 class Kernel
 {
     public function commands(): array
@@ -27,7 +37,7 @@ class Kernel
     public function schedule()
     {
         $tasks = [
-//            (new ScheduledTask('TestJob', 'run'))->everyMinute(),
+            (new ScheduledTask('TestJob', 'run'))->everyMinute(),
 //            (new ScheduledTask('TestJob', 'run'))->dailyAt('03:00'),
 //            (new ScheduledTask('TestJob', 'handle'))->dailyAt('03:00'),
         ];
